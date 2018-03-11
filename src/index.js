e334fb28ed4925a6d964037d0c3214e1b1cca04f
Alexandr <34417026+KamenkoAlexandr@users.noreply.github.com>
2018-03-11 20:15:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,39 @@
 
 const createEnumerableProperty = () => {};
 const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
+const createProtoMagicObject = () => {
+    var object = Object;
+    object.__proto__ = object.prototype;
+    return object;
+};
+
 const incrementor = () => {};
-const asyncIncrementor = () => {};
+
+var async = 0;
+const asyncIncrementor = () => {
+    async++;
+    return async;
+};
+
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = (param) => {};
+
+
+const getDeepPropertiesCount = (object) => {
+    return JSON.stringify(object).match(/{/g).length-1;
+};
+const createSerializedObject = () => {
+    return null;
+};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (ar) => {
+    function compare(x,y) {
+        return x.__proto__ - y.__proto__;
+    }
+    return ar.sort(compare);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
